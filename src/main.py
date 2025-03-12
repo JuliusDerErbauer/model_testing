2cbec0478cc034a6e20457ecf5f144a8e82ee3cb
JuliusDerErbauer <95232801+JuliusDerErbauer@users.noreply.github.com>
2025-03-12 18:03:18 -0500

    edited chamfer loss

--- src/main.py
+++ src/main.py
@@ -21,8 +21,10 @@ class ChamferLoss(nn.Module):
         super(ChamferLoss, self).__init__()
         self.loss = ChamferDistance()
 
-    def forward(self, *args):
-        loss = self.loss(*args)[0]
+    def forward(self, x, y):
+        x = x.permute(0, 2, 1)
+        y = y.permute(0, 2, 1)
+        loss = self.loss(x, y)[0]
         return loss.mean()
 
 
