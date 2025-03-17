488150d5432620b0c245515c769b9920734f6b5c
JuliusDerErbauer <95232801+JuliusDerErbauer@users.noreply.github.com>
2025-03-17 09:54:26 -0500

    Tried something with CD

--- src/model/chamfer.py
+++ src/model/chamfer.py
@@ -13,4 +13,4 @@ class ChamferLoss(nn.Module):
         y = y.permute(0, 2, 1)
         dist_x, dist_y, indices_x, indices_y = self.loss(x, y)
 
-        return torch.mean(dist_x) + torch.mean(dist_y)
+        return torch.mean(dist_x) + torch.mean(dist_y) * 10
